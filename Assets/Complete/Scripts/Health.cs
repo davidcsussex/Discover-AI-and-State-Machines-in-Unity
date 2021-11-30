3a35b75ff15c6a523f19d5f265247e4bb28790d7
davidcsussex <david.clarke@escg.ac.uk>
2021-11-30 18:35:01 +0000

    Initial commit

---
+++ Assets/Complete/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    private int maxHealth;
+    private int currentHealth;
+    [SerializeField]
+    private UnityEngine.UI.Slider healthbar;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void SetHealth(int amount)
+    {
+        currentHealth = maxHealth = amount;
+        OnHealthChange();
+    }
+    
+    void Heal(int amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        OnHealthChange();
+    }
+    void OnHealthChange()
+    {
+        healthbar.value = (float)currentHealth / (float)maxHealth;
+    }
+
+    public IEnumerator TakeDamageDelayed(int amount, float delay)
+    {
+
+        yield return new WaitForSeconds(delay);
+        currentHealth = Mathf.Max(currentHealth - amount, 0);
+        if (currentHealth == 0)
+        {
+            SetHealth(20);
+        }
+        OnHealthChange();
+    }
+}
